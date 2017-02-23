cccc1c8296fe6624cdf56701970ea25ffb42aa22
Fergus <ferguslongley@live.com>
2017-02-23 20:34:15 +0000

    Updated testing file

--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,7 @@
 import os
-import sys
 import unittest
 import pocketcasts
 
-sys.path.insert(0, os.path.dirname(__file__))
 
 USERNAME = os.environ.get('POCKETCAST_USER')
 PASSWORD = os.environ.get('POCKETCAST_PASSWORD')
